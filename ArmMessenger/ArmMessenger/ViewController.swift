e308b929828b0fb0d0a3041d7db3ec6f9d981864
Aswin Rathees <33830946+aswinrathees@users.noreply.github.com>
2025-01-15 23:24:55 +0530

    Initial project setup

---
+++ ArmMessenger/ArmMessenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArmMessenger
+//
+//  Created by Aswin R on 11/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ArmMessenger/ArmMessengerTests/ArmMessengerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ArmMessengerTests.swift
+//  ArmMessengerTests
+//
+//  Created by Aswin R on 11/01/25.
+//
+
+import Testing
+@testable import ArmMessenger
+
+struct ArmMessengerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
